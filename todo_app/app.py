23fb39a7c522f6e253f66165177d1f5ba30427c6
Dawei <dz4@pdx.edu>
2020-05-30 01:20:37 -0700

    implement UI for 1_to_many

--- todo_app/app.py
+++ todo_app/app.py
@@ -26,9 +26,16 @@ class TodoList(db.Model):
 
 #db.create_all()
 
+@app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+    return render_template('index.html', 
+    lists=TodoList.query.all(), 
+    active_list=TodoList.query.get(list_id),
+    todos=Todo.query.filter_by(list_id=list_id).order_by('id').all())
+
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.order_by('id').all())
+    return redirect(url_for('get_list_todos', list_id=1))
 
 @app.route('/todos/<todo_id>', methods=['DELETE'])
 def delete_todo(todo_id):
