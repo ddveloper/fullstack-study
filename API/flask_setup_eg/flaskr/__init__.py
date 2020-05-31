5f13467394ed1341191aa5389f952d313e72a74d
Dawei Zhang <dz4@pdx.edu>
2020-05-31 01:33:36 -0700

    simple flask

---
+++ API/flask_setup_eg/flaskr/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+
+    @app.route('/')
+    def hello():
+        return jsonify({'message': 'Hello API'})
+  
+    @app.route('/smiley')
+    def smiley():
+        return ': )'
+
+    
+    return app
