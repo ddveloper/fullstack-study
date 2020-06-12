9af74dfaaf78fae8a03ab32cfe2888b20f311459
Dawei Zhang <dz4@pdx.edu>
2020-06-12 11:22:06 -0700

    [API] initial setup, add entries in model

--- API/flask_setup_eg/flaskr/__init__.py
+++ API/flask_setup_eg/flaskr/__init__.py
@@ -1,15 +1,27 @@
 from flask import Flask, jsonify
+from models import setup_db
+from flask_cors import CORS
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    setup_db(app)
+    CORS(app) # CORS(app, resources={r"*/api/*": {origins: '*'}})
+  
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        response.headers.add('Access-Control-Allow-Headers', 'GET, POST, PATCH, DELETE, OPTIONS')
+        return response
 
     @app.route('/')
+    # @cross_origin
     def hello():
         return jsonify({'message': 'Hello API'})
-  
-    @app.route('/smiley')
-    def smiley():
-        return ': )'
 
-    
     return app
+
+
+# Default port:
+if __name__ == '__main__':
+    app = create_app()
+    app.run()
